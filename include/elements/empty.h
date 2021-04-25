1b9d281b2ef5b68e9d53123a2e09b6c30d807ae4
martinapz <martina8@illinois.edu>
2021-04-25 17:17:11 -0500

    Created classes for each of the Static objects on the board.

---
+++ include/elements/empty.h
@@ -0,0 +1,25 @@
+#ifndef FINAL_PROJECT_EMPTY_H
+#define FINAL_PROJECT_EMPTY_H
+
+#include "element_interfaces.h"
+
+namespace finalproject {
+namespace elements {
+
+    /**
+    * Class that represents the Dot object in the Pacman game.
+    */
+    class Empty : public StaticElement {
+    public:
+        Empty() = default;
+
+        void draw(size_t row, size_t col) const;
+    private:
+        ci::Color color_ = ci::Color("black");
+
+    };
+
+} //namespace elements
+} //namespace finalproject
+
+#endif //FINAL_PROJECT_EMPTY_H
