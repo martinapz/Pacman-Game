2dfd17c946a154645b2164e1fb8d963076687b0f
martinapz <martina8@illinois.edu>
2021-04-25 17:18:00 -0500

    Added direction enum.

--- include/types.h
+++ include/types.h
@@ -4,22 +4,26 @@
 #include <string>
 
 namespace finalproject {
-        struct Configuration {
-            constexpr static const double WINDOW_SIZE = 875;
-            constexpr static const double WINDOW_MARGIN = 100;
-            constexpr static const size_t GRID_SIZE = 23;
-            const std::string LEVEL_DATA_FILE = "C:\\Users\\marti\\CLionProjects\\cinder_master\\My-Projects\\final-project-martinapz\\data\\level1.txt";
-        };
+    struct Configuration {
+        constexpr static const double WINDOW_SIZE = 875;
+        constexpr static const double WINDOW_MARGIN = 100;
+        constexpr static const size_t GRID_SIZE = 23;
+        const std::string LEVEL_DATA_FILE = "C:\\Users\\marti\\CLionProjects\\cinder_master\\My-Projects\\final-project-martinapz\\data\\level1.txt";
+    };
 
-        enum ELEMENT {
-            PACMAN,
-            WALL,
-            DOT,
-            COIN,
-            GHOST,
-            DOOR,
-            EMPTY
-        };
+    enum Action {
+        START,
+        EXIT,
+        PAUSE
+    };
+
+    enum Direction {
+        LEFT,
+        RIGHT,
+        UP,
+        DOWN,
+        NONE
+    };
 }
 
 #endif //FINAL_PROJECT_TYPES_H
